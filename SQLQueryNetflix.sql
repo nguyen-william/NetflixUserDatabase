79ba052b7c1b570ce6a233388c5b2ec332aace54
nguyen-william <77467480+nguyen-william@users.noreply.github.com>
2024-05-31 22:35:00 -0400

    added file
    cleaning Netflix user data for Power Bi

---
+++ SQLQueryNetflix.sql
@@ -0,0 +1,23 @@
+
+SELECT 
+    [User_ID]
+    [Subscription_Type],
+    [Monthly_Revenue],
+    [Join_Date],
+    [Last_Payment_Date],
+    [Country],
+    [Age],
+    [Gender],
+    [Device],
+    [Plan_Duration],
+    CASE 
+        WHEN [Age] BETWEEN 18 AND 25 THEN '18-25'
+        WHEN [Age] BETWEEN 26 AND 35 THEN '26-35'
+        WHEN [Age] BETWEEN 36 AND 45 THEN '36-45'
+        WHEN [Age] BETWEEN 46 AND 55 THEN '46-55'
+        WHEN [Age] BETWEEN 56 AND 65 THEN '56-65'
+        ELSE '65+'
+    END AS Age_Range
+FROM 
+    [Netflix Userbase] 
+
